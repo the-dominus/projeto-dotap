1de3440698c71293c2d4f6fb54dde8bf2f779645
Cleyson Silva <cleysonsilva.me@gmail.com>
2022-11-02 20:39:52 -0300

    add dumps

---
+++ dumps-dotap/dotap_db_solicitacoes_registros_altera.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE  IF NOT EXISTS `dotap_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dotap_db`;
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dotap_db
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `solicitacoes_registros_altera`
+--
+
+DROP TABLE IF EXISTS `solicitacoes_registros_altera`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `solicitacoes_registros_altera` (
+  `id_solicitacao` int NOT NULL,
+  `id_registro` int NOT NULL,
+  PRIMARY KEY (`id_solicitacao`,`id_registro`),
+  KEY `FK_solicitacoes_registros_altera_3_idx` (`id_registro`),
+  CONSTRAINT `FK_solicitacoes_registros_altera_2` FOREIGN KEY (`id_solicitacao`) REFERENCES `solicitacoes` (`id`),
+  CONSTRAINT `FK_solicitacoes_registros_altera_3` FOREIGN KEY (`id_registro`) REFERENCES `registros` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitacoes_registros_altera`
+--
+
+LOCK TABLES `solicitacoes_registros_altera` WRITE;
+/*!40000 ALTER TABLE `solicitacoes_registros_altera` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitacoes_registros_altera` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-02 20:37:34
